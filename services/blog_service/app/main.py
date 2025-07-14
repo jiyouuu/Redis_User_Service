d195b148f6f75425cabd586c1eb204cc01f02983
jiyou <wldb2065@gmail.com>
2025-07-14 16:38:01 +0900

    자잘한 거 수정

--- services/blog_service/app/main.py
+++ services/blog_service/app/main.py
@@ -1,4 +1,19 @@
 import os
 from fastapi import FastAPI
 from database import init_db, get_session
+from models import ArticleImage,BlogArticle
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    print("🚀 서버 시작 중... DB 초기화 중")
+    await init_db()  # 비동기 DB 초기화
+    print("✅ DB 초기화 완료")
+
+    yield  # 여기가 실행되면 FastAPI 앱이 본격적으로 실행됨
+
+    # shutdown
+    print("🛑 서버 종료!")
+
+app = FastAPI(title = "User Service", lifespan=lifespan)
